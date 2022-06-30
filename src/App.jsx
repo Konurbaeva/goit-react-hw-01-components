9d71b2be324a5efd1b799455f98966deaf3a7fd0
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-06-30 23:26:31 +0200

    moved mock to src

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@
 // import { Statistics } from './Statistics';
 // import { FriendList } from './FriendList';
 // import { TransactionHistory } from './TransactionHistory';
-// import user from './mock/user.json';
-// import data from './mock/data.json';
-// import friends from './mock/friends.json';
-// import transactions from './mock/transactions.json';
 
 import { Profile } from './components/Profile'
 import { Statistics } from './components/Statistics';
 import { FriendList } from './components/FriendList';
 import { TransactionHistory } from './components/TransactionHistory';
-import user from './components/mock/user.json';
-import data from './components/mock/data.json';
-import friends from './components/mock/friends.json';
-import transactions from './components/mock/transactions.json';
+
+import user from './mock/user.json';
+import data from './mock/data.json';
+import friends from './mock/friends.json';
+import transactions from './mock/transactions.json';
 
 
 export const App = () => {

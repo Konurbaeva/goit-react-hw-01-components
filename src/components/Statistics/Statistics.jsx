8aaf9ffec0a5a0cbddd1fc3221f03f8d04daf150
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-02 20:43:32 +0200

    center stats

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,22 +2,17 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { generateRandomHexColor } from 'utils/generateRandomHexColor';
 
-// export const SectionUl = styled.ul`
-//   display: flex;
-//   padding: 20px;
-//   width: 20%;
-//   margin: 0 auto;
-//   & {
-//     list-style: none;
-//   }
-// `;
+export const SectionUl = styled.ul`
+  display: flex;
+  padding: 20px;
+  width: 20%;
+  height: 50%;
+  margin: 0 auto;
+`;
 
 export const SectionLi = styled.li`
   background-color: ${generateRandomHexColor};
   width: 50%;
-  & {
-    list-style: none;
-  }
 `;
 
 export const Statistics = ({ title, stats }) => {
@@ -25,14 +20,14 @@ export const Statistics = ({ title, stats }) => {
     <>
       <section className="statistics">
         {title && <h2 className="title">Upload stats</h2>}
-        <ul className="stat-list">
+        <SectionUl className="stat-list">
           {stats.map(({ id, label, percentage }) => (
             <SectionLi className="item" key={id}>
               <span className="label">{label}</span>
               <span className="percentage">{percentage}</span>
             </SectionLi>
           ))}
-        </ul>
+        </SectionUl>
       </section>
     </>
   );

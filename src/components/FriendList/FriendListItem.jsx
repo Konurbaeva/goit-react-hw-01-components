f776640adc0be1a35f978c35b042b14087368a19
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-02 15:34:15 +0200

    adjust styling

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,27 +1,9 @@
 import styled from 'styled-components';
 
-// const ListItem = styled.li(({ isOnline }) => ({
-//   color: isOnline ? 'green' : 'red',
-//   width: '30px',
-// }));
-
-// export const FriendListItem = ({ friend }) => {
-//   const { name, isOnline, avatar, id } = friend;
-
-//   return (
-//     <>
-//       <ListItem className="item" key={id} isOnline={isOnline}>
-//         <img className="avatar" src={avatar} alt="User avatar" width="48" />
-//         <span>{name}</span>
-//       </ListItem>
-//     </>
-//   );
-// };
-
-// const ListItem = styled.li(({ isOnline }) => ({}));
+// В зависимости от пропа isOnline, должен меняться цвет фона span.status.
 
 export const ListItem = styled.li`
-  background-color: ${props => (props.isOnline ? 'green' : 'red')};
+  color: ${props => (props.isOnline ? 'green' : 'red')};
 `;
 
 export const FriendListItem = ({ friend }) => {
@@ -30,9 +12,10 @@ export const FriendListItem = ({ friend }) => {
   return (
     <>
       <ListItem className="item" key={id} isOnline={isOnline}>
+        <span className="status"></span>
         <img className="avatar" src={avatar} alt="User avatar" width="48" />
-        <span>{name}</span>
       </ListItem>
+      <p>{name}</p>
     </>
   );
 };

80b41ab0e72d7ea2084f72c0522897a3ba349e91
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-02 20:10:49 +0200

    fixed online status color

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,22 +1,12 @@
 import { Card } from 'components/ui/card';
 import styled from 'styled-components';
 
-// В зависимости от пропа isOnline, должен меняться цвет фона span.status.
-
-export const ListItem = styled.li`
-  color: ${props => (props.isOnline ? 'green' : 'red')};
-  .status {
-    // position: absolute;
-    background-color: #ff0000;
-    border-radius: 50%;
-    width: 1%;
-    height: 1%;
-    border: #fff 3px solid;
-    bottom: 3px;
-  }
-  & {
-    list-style: none;
-  }
+export const Span = styled.span`
+  border: ${props => (props.isOnline ? '#33cc33' : '#ff0000')} 5px solid;
+  border-radius: 50%;
+  width: 20%;
+  height: 20%;
+  bottom: 3px;
 `;
 
 export const FriendListItem = ({ friend }) => {
@@ -25,12 +15,28 @@ export const FriendListItem = ({ friend }) => {
   return (
     <>
       <Card>
-        <ListItem className="item" key={id}>
-          <span className="status" isOnline={isOnline}></span>
+        <li className="item" key={id}>
+          <Span className="status" isOnline={isOnline}></Span>
           <img className="avatar" src={avatar} alt="User avatar" width="48" />
-        </ListItem>
+        </li>
         <p>{name}</p>
       </Card>
     </>
   );
 };
+
+// export const FriendListItem = ({ friend }) => {
+//   const { name, isOnline, avatar, id } = friend;
+
+//   return (
+//     <>
+//       <Card>
+//         <ListItem className="item" key={id}>
+//           <span className="status" isOnline={isOnline}></span>
+//           <img className="avatar" src={avatar} alt="User avatar" width="48" />
+//         </ListItem>
+//         <p>{name}</p>
+//       </Card>
+//     </>
+//   );
+// };

fa2edd45a9432a5af9c2f1d97a4663ac66360dcb
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-02 20:54:05 +0200

    added nth-child for TableRow

--- src/components/ui/table/TableRow.jsx
+++ src/components/ui/table/TableRow.jsx
@@ -2,5 +2,10 @@ import styled from 'styled-components';
 
 export const TableRow = styled.tr`
   border-bottom: 1px solid ${({ theme }) => theme.colors.primary};
-  background-color: ${props => (props.red ? 'white' : 'yellow')};
+  &:nth-child(odd) {
+    background: green;
+  }
+  &:nth-child(even) {
+    background: pink;
+  }
 `;

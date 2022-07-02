486b716fb510782c93734d9e76a80f74bbb7f26a
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-02 15:12:43 +0200

    added props case for isOnline part1

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,9 +1,28 @@
 import styled from 'styled-components';
 
-const ListItem = styled.li(({ isOnline }) => ({
-  color: isOnline ? 'green' : 'red',
-  width: '30px',
-}));
+// const ListItem = styled.li(({ isOnline }) => ({
+//   color: isOnline ? 'green' : 'red',
+//   width: '30px',
+// }));
+
+// export const FriendListItem = ({ friend }) => {
+//   const { name, isOnline, avatar, id } = friend;
+
+//   return (
+//     <>
+//       <ListItem className="item" key={id} isOnline={isOnline}>
+//         <img className="avatar" src={avatar} alt="User avatar" width="48" />
+//         <span>{name}</span>
+//       </ListItem>
+//     </>
+//   );
+// };
+
+// const ListItem = styled.li(({ isOnline }) => ({}));
+
+export const ListItem = styled.li`
+  background-color: ${props => (props.isOnline ? 'green' : 'red')};
+`;
 
 export const FriendListItem = ({ friend }) => {
   const { name, isOnline, avatar, id } = friend;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,15 +2,15 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { generateRandomHexColor } from 'utils/generateRandomHexColor';
 
-export const SectionUl = styled.ul`
-  display: flex;
-  padding: 20px;
-  width: 20%;
-  margin: 0 auto;
-  & {
-    list-style: none;
-  }
-`;
+// export const SectionUl = styled.ul`
+//   display: flex;
+//   padding: 20px;
+//   width: 20%;
+//   margin: 0 auto;
+//   & {
+//     list-style: none;
+//   }
+// `;
 
 export const SectionLi = styled.li`
   background-color: ${generateRandomHexColor};
@@ -21,14 +21,14 @@ export const Statistics = ({ title, stats }) => {
     <>
       <section className="statistics">
         {title && <h2 className="title">Upload stats</h2>}
-        <SectionUl className="stat-list">
+        <ul className="stat-list">
           {stats.map(({ id, label, percentage }) => (
             <SectionLi className="item" key={id}>
               <span className="label">{label}</span>
               <span className="percentage">{percentage}</span>
             </SectionLi>
           ))}
-        </SectionUl>
+        </ul>
       </section>
     </>
   );

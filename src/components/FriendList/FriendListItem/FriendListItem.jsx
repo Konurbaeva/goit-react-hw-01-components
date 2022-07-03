1dc78a401b965fc0cc835c775a42ee5b877ab390
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-03 20:45:45 +0200

    changing FriendListItem styling

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -2,11 +2,19 @@ import { Card } from 'components/ui/card';
 import styled from 'styled-components';
 
 export const Span = styled.span`
-  border: ${props => (props.isOnline ? '#35bb23' : '#ff0000')} 10px solid;
+  /* border: ${props => (props.isOnline ? '#35bb23' : '#ff0000')} 10px solid; */
+  border: ${props => (props.isOnline ? '#35bb23' : '#ff0000')} 5px solid;
   margin-right: 10px;
-  width: 20%;
-  height: 20%;
+  width: 10%;
+  height: 10%;
   border-radius: 50%;
+  color: pink;
+`;
+
+export const ListItem = styled.li`
+  height: 60px;
+  margin-top: 15px;
+  vertical-align: center;
 `;
 
 export const FriendListItem = ({ friend }) => {
@@ -15,11 +23,16 @@ export const FriendListItem = ({ friend }) => {
   return (
     <>
       <Card>
-        <li className="item" key={id}>
+        <ListItem className="item" key={id}>
           <Span className="status" isOnline={isOnline}></Span>
-          <img className="avatar" src={avatar} alt="User avatar" width="48" />
-        </li>
-        <p>{name}</p>
+          <img
+            className="avatar"
+            src={avatar}
+            alt="User avatar"
+            width="48"
+          />{' '}
+          {name}
+        </ListItem>
       </Card>
     </>
   );
--- src/components/ui/card/Card.js
+++ src/components/ui/card/Card.js
@@ -1,14 +1,10 @@
 import styled from 'styled-components';
 
 export const Card = styled.div`
-padding: 10px;
 margin: 0 auto;
-margin-top: 10px;
-transition: 0.3s;
 width: 20%;
-align-items: center;
+border-radius: 10px;
 box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-border-radius: 30px;
 &:hover {
     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     }
